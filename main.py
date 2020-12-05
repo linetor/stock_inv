2e9d5e65428f71193e3d8d76de575ea064a715dd
linetor <linetor@gmail.com>
2020-12-06 00:58:51 +0900

    [linetor-001]  insert daily stock by code method todo : daemon adding : http://blog.cloudsys.co.kr/python-daemon-example-install/

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ from mongodb import MongoManager
 from stockCrawling import StockCrawling
 
 if __name__ == "__main__":
+    import pandas as pd
     stockInfor = StockCrawling()
-    print(stockInfor.insertCompanyInfor())
-    print(stockInfor.getCompanySize())
+    #print(stockInfor.insertCompanyInfor())
+    #print(stockInfor.getCompanySize())
+    #print(stockInfor.getStockByCode())
+    print(stockInfor.insertDailyStockByCode(10))
+    """
+    url = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=str(5930).zfill(6) )
+    url = '{url}&page={page}'.format(url=url, page=1)
+    print(url)
+    df = pd.DataFrame()
+    df = df.append(pd.read_html(url, header=0)[0], ignore_index=True)
+    print(df)
+    print(df.count())
+
+    from bs4 import BeautifulSoup
+    import requests
+    result=requests.get(url)
+
+    bs=BeautifulSoup(result.content,"html.parser")
+    print("Beautiful",bs)
+    salary = bs.find_all("td", class_="pgRR")[-1]  # last cell in the row
+    print("salary",salary)
+    print("salary : hred",salary.find_all('a', href=True)[-1]['href'])
+    print("salary : href -1 ",salary.find_all('a', href=True)[-1]['href'].split("=")[-1])
+
+    print("salary.text",salary.get_text())
+    bs.find("p")
+    """
+
+
+
+
+
+
+
--- stockCrawling.py
+++ stockCrawling.py
@@ -52,7 +52,79 @@ class StockCrawling:
     def getCompanySize(self):
         return self.mongoclient.companyinfor.count()
 
-    def getStockByCode(self):
-        return None
-        # to do
+    def insertDailyStockByCode(self,codeNum):
 
+        getStockInfor = self.mongoclient.companyinfor.find_one({ "code": codeNum })
+        print(getStockInfor)
+        if(getStockInfor == None):
+            return "code:"+str(codeNum)+" not valid"
+
+        import pymongo
+        getStockDailyLastDate = self.mongoclient.dailystockprice.find_one ( { "code" : codeNum},sort=[("date", pymongo.DESCENDING)])
+
+        df = pd.DataFrame()
+        urlOriginal = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code= str(codeNum).zfill(6))
+        url = '{url}&page={page}'.format(url=urlOriginal, page=1)
+
+        lastdate = '1900-01-01'
+        pageNum = 0
+        from bs4 import BeautifulSoup
+        import requests
+        print(getStockDailyLastDate)
+
+
+        if(getStockDailyLastDate != None ):
+            print("if condition")
+            import datetime
+            from datetime import date
+            import numpy as np
+            print("Not None",getStockDailyLastDate)
+            lastdate = datetime.datetime.fromtimestamp(getStockDailyLastDate['date']/1000 + 60*60*24).strftime('%Y-%m-%d')
+            curdate = date.today().strftime("%Y-%m-%d")
+            print("checking date",lastdate,curdate)
+            weekdaydiff = np.busday_count(lastdate, curdate)
+            print("date diff",weekdaydiff)
+            if weekdaydiff > 0 :
+                pageNum = (weekdaydiff-1) // 10 + 1
+
+        else :
+            print("else condition")
+            result=requests.get(url)
+            bs = BeautifulSoup(result.content,"html.parser")
+            listChecking=bs.find_all("td", class_="pgRR")
+            if(len(listChecking)==0):
+                return "code:"+str(codeNum)+" not recorded"
+            pageNum = int(bs.find_all("td", class_="pgRR")[-1].find_all('a', href=True)[-1]['href'].split("=")[-1])
+
+        print("page check",pageNum)
+        import time
+        import random
+        for pageNum in range(1,pageNum+1):
+            url = '{url}&page={page}'.format(url=urlOriginal, page=pageNum)
+            df = df.append(pd.read_html(url, header=0)[0].dropna(axis=0), ignore_index=True)
+            sleepTime = (1000 + round(random.uniform(-1, 1) * 500))/1000
+            print(pageNum,"sleep",sleepTime)
+            time.sleep(sleepTime)
+
+
+        print("df count check" , df.size )
+
+        if len(df) > 0 :
+            df = df.rename(columns= {'날짜': 'date', '종가': 'close', '전일비': 'diff', '시가': 'open', '고가': 'high', '저가': 'low', '거래량': 'volume'})
+
+            df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close', 'diff', 'open', 'high', 'low', 'volume']].astype(int)
+            df['strdate'] = df['date']
+            df = df[df['strdate'] > lastdate]
+            df['date'] = pd.to_datetime(df['date'])
+            df['code'] = codeNum
+            df = df.sort_values(by=['date'], ascending=True)
+            df = df.reset_index(drop=True)
+
+            print("df",df)
+            print("df.count",df.count())
+            records = json.loads(df.T.to_json()).values()
+            self.mongoclient.dailystockprice.insert(records)
+
+        return "code:"+str(codeNum)+" insert : "+ str(len(df))
+        # to do
+    #self.mongoclient.dailystockprice.count()
